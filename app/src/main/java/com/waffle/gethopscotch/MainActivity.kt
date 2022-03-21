2d626499126d987d6ecfa37e20befa42125a7999
quitd <degrou99@gmail.com>
2022-03-21 08:33:15 +0700

    uh

--- app/src/main/java/com/waffle/gethopscotch/MainActivity.kt
+++ app/src/main/java/com/waffle/gethopscotch/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.waffle.gethopscotch
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -18,12 +18,17 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
+import com.beust.klaxon.Klaxon
 import com.waffle.gethopscotch.ui.theme.HopscotchTheme
+import okhttp3.*
+import java.io.IOException
+
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT //probably temp?
         setContent {
             HopscotchTheme {
                 val navController = rememberNavController()
@@ -70,8 +75,49 @@ fun Home(n: NavController) {
     }
 }
 
+class Project(
+    val objects: Array<Object>,
+    val scenes: Array<Scene>,
+    val stageSize: StageSize,
+)
+
+class Object(
+    val type: Int,
+    val objectID: String,
+    var text: String?,
+    val xPosition: String,
+    val yPosition: String,
+)
+
+class Scene(
+    val objects: Array<String>,
+    val name: String,
+)
+
+class StageSize(
+    val width: Int,
+    val height: Int,
+)
+
 @Composable
 fun Editor() {
+    var project: Project? by remember { mutableStateOf(null) }
+
+    val client = OkHttpClient()
+    val req = Request.Builder()
+        .url("https://c.gethopscotch.com/api/v1/projects/115dag2ang")
+        .get()
+        .build()
+    client.newCall(req).enqueue(object : Callback {
+        override fun onResponse(call: Call, response: Response) {
+            project = Klaxon().parse<Project>(response.body!!.string())!!
+        }
+
+        override fun onFailure(call: Call, e: IOException) {
+            TODO("Not yet implemented")
+        }
+    })
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -80,15 +126,46 @@ fun Editor() {
         }
     ) {
         Surface(
-            color = MaterialTheme.colors.background,
-            modifier = Modifier.fillMaxSize()
+            color = MaterialTheme.colors.background
         ) {
-            AsyncImage(
-                model = R.drawable.editor_bg,
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
+            if (project != null) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .width(project!!.stageSize.width.dp)
+                            .height(project!!.stageSize.height.dp)
+                            .aspectRatio((project!!.stageSize.width.dp / project!!.stageSize.height.dp)),
+                    ) {
+                        AsyncImage(
+                            model = R.drawable.editor_bg,
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop
+                        )
+                        project!!.scenes[0].objects.forEach { v ->
+                            val obj = project!!.objects.find { it.objectID == v }
+                            if (obj?.type == 1) {
+                                Text(
+                                    obj.text!!,
+                                    modifier = Modifier.offset(obj.xPosition.toFloat().dp,
+                                        obj.yPosition.toFloat().dp),
+                                    color = Color.Black
+                                )
+                            }
+                        }
+                    }
+                }
+            } else {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
         }
     }
 }
