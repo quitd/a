ac731d0d8fc4945fe9e625e5097c4220fe138a74
quitd <degrou99@gmail.com>
2022-05-10 16:46:11 +0700

    aaaaaa

--- app/src/main/java/com/waffle/gethopscotch/MainActivity.kt
+++ app/src/main/java/com/waffle/gethopscotch/MainActivity.kt
@@ -4,9 +4,7 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -34,7 +32,6 @@ import okhttp3.*
 import java.io.IOException
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT //probably temp?
@@ -85,11 +82,14 @@ fun Home(n: NavController) {
 }
 
 class Project(
-    val objects: Array<Object>,
-    val scenes: Array<Scene>,
-    val stageSize: StageSize,
-    val rules: Array<Rule>,
-    val customRules: Array<CustomRule>
+    var objects: Array<Object>,
+    var scenes: Array<Scene>,
+    var stageSize: StageSize,
+    var rules: Array<Rule>,
+    var customRules: Array<CustomRule>,
+    var abilities: Array<Ability>,
+    var eventParameters: Array<ep>,
+    var variables: Array<Var>
 )
 
 class Object(
@@ -116,14 +116,14 @@ class Param(
     val value: String,
     var datum: Datum?,
     var name: String?,
-    /*val type: Int,
-    val key: String,
     var variable: String?,
-    var params: Array<Param>?*/
+    val key: String,
+    var params: Array<Param>?,
+    val type: Int
 )
 
 class Rule(
-    //val abilityID: String,
+    val abilityID: String,
     val id: String,
     val parameters: Array<Param>?
 )
@@ -136,15 +136,42 @@ class CustomRule(
 
 class Datum(
     val type: Int,
-    /*var variable: String?,
-    var HSTraitTypeKey: Int?,
+    var variable: String?,
+    var params: Array<Param>?,
+    var description: String?,
+    /*var HSTraitTypeKey: Int?,
     var HSTraitObjectIDKey: String?,
     var HSTraitIDKey: String?,
     var HSTraitObjectParameterTypeKey: Int?,
-    var obj: Obj?,
-    var description: String?,
-    var params: Array<Param>?*/
+    var obj: Obj?,*/
 )
+
+class Ability(
+    var abilityID: String,
+    var blocks: Array<Bloc>
+)
+
+class Bloc(
+    var type: Int,
+    var parameters: Array<Param>?,
+    var controlScript: cS,
+    var controlFalseScript: cS
+)
+
+class ep(
+    var blockType: Int,
+    var description: String,
+    var id: String,
+    var objectID: String?
+)
+
+class Var(
+    var name: String,
+    var objectIdString: String,
+    var type: Int
+)
+
+class cS(var abilityID: String)
 
 @Composable
 fun Editor() {
@@ -152,7 +179,7 @@ fun Editor() {
 
     val client = OkHttpClient()
     val req = Request.Builder()
-        .url("https://c.gethopscotch.com/api/v1/projects/11r0coc6ew")
+        .url("https://c.gethopscotch.com/api/v1/projects/115dag2ang")
         .get()
         .build()
     client.newCall(req).enqueue(object : Callback {
@@ -179,7 +206,8 @@ fun Editor() {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "objects") {
                     composable("objects") { objE(project!!, navController) }
-                    composable("code/{obj}",
+                    composable(
+                        "code/{obj}",
                         arguments = listOf(navArgument("obj") {
                             type = NavType.StringType
                         })
@@ -218,15 +246,16 @@ fun objE(project: Project, n: NavController) {
             project.scenes[0].objects.forEach { v -> //scene
                 val obj = project.objects.find { it.objectID == v }
                 if (obj?.type == 1) {
+                    val empty = obj.text!!.isNotEmpty()
                     Text(
-                        obj.text!!,
+                        if (empty) obj.text!! else "Text",
                         modifier = Modifier
                             .offset(
-                                obj.xPosition.toFloat().dp,
+                                (obj.xPosition.toFloat() / project.stageSize.width).dp,
                                 obj.yPosition.toFloat().dp
                             )
                             .clickable { n.navigate("code/" + obj.objectID) },
-                        color = Color.Black
+                        color = if (empty) Color.Black else Color.Gray
                     )
                 }
             }
@@ -252,8 +281,13 @@ fun codeE(ba: NavBackStackEntry, project: Project) {
                         .padding(10.dp)
                         .fillMaxSize()
                 ) {
-                    obj.rules.forEach {
-                        rule(it, project)
+                    Column(
+                        Modifier.verticalScroll(rememberScrollState()),
+                        verticalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
+                        obj.rules.forEach {
+                            rule(it, project)
+                        }
                     }
                 }
             }
@@ -275,7 +309,11 @@ fun rule(id: String, project: Project) {
         ) {
             Column {
                 Row(
-                    if (open) Modifier.padding(bottom = 10.dp) else Modifier,
+                    if (open) Modifier
+                        .padding(bottom = 10.dp)
+                        .horizontalScroll(rememberScrollState()) else Modifier.horizontalScroll(
+                        rememberScrollState()
+                    ),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     val col = if (p is CustomRule) Color.Black else Color.White
@@ -291,13 +329,12 @@ fun rule(id: String, project: Project) {
 
                     Text(
                         if (p is CustomRule) p.name else "When",
-                        Modifier.padding(end = 8.dp),
                         color = col
                     )
 
                     if (p is Rule) {
                         p.parameters!!.forEach {
-                            Par(it)
+                            Par(it, project, true)
                         }
                     }
                 }
@@ -315,6 +352,14 @@ fun rule(id: String, project: Project) {
                                 p.rules.forEach {
                                     rule(it, project)
                                 }
+                            } else if (p is Rule) {
+                                Column(
+                                    verticalArrangement = Arrangement.spacedBy(6.dp)
+                                ) {
+                                    project.abilities.find { it.abilityID == p.abilityID }?.blocks?.forEach {
+                                        Block(it, project)
+                                    }
+                                }
                             }
                         }
                     }
@@ -325,12 +370,116 @@ fun rule(id: String, project: Project) {
 }
 
 @Composable
-fun Par(it: Param) {
-    Box(
-        Modifier
-            .border(2.dp, Color.White, CircleShape)
-            .padding(4.dp)) {
-        if (it.datum != null)
-            Text(blocks[it.datum?.type.toString()]?.get(1) ?: "", color = Color.White)
+fun Par(it: Param, project: Project, first: Boolean = false) {
+    Box(Modifier.padding(start = 6.dp)) {
+        var k = ""
+        var color: Boolean by remember { mutableStateOf(false) }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(k, Modifier.padding(end = 4.dp), color = if (first) Color.White else Color.Black)
+            Box(
+                Modifier
+                    .background(if (color) Color(255, 165, 0) else Color.White, CircleShape)
+                    .border(2.dp, Color.Black, CircleShape)
+                    .padding(12.dp, 4.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    val str: ArrayList<Unit> = arrayListOf()
+                    if (it.datum != null) {
+                        if (it.datum?.variable != null) {
+                            val va =
+                                project.variables.find { v -> v.objectIdString == it.datum?.variable }
+                            val ei = va?.type != 8000
+                            str.add(
+                                Text(
+                                    "${if (ei) blocks[va?.type.toString()]?.get(0) else ""} ${va?.name!!}",
+                                    color = if (ei) Color.White else Color.Black
+                                )
+                            )
+                            color = ei
+                        } else if (it.datum?.params != null) {
+                            k =
+                                if (blocks[it.datum?.type.toString()]?.get(1) != it.datum?.params!![1].key) blocks[it.datum!!.type.toString()]?.get(
+                                    1
+                                ) ?: it.key
+                                else it.key
+                            it.datum!!.params?.forEach {
+                                str.add(Par(it, project))
+                            }
+                        } else {
+                            val s = blocks[it.datum?.type.toString()]
+                            str.add(Text(s?.getOrElse(1) { s[0] } ?: "", color = Color.Black))
+                        }
+                    } else {
+                        if (it.params != null) {
+                            k = blocks[it.type.toString()]?.get(1).toString()
+                            it.params?.forEach {
+                                str.add(Par(it, project))
+                            }
+                        } else {
+                            k = it.key
+                            str.add(Text(it.value, color = Color.Black))
+                            if (it.variable != null) {
+                                val f = project.eventParameters.find { v -> v.id == it.variable }
+                                when (f?.blockType) {
+                                    8000 -> {
+                                        str.add(
+                                            Text(
+                                                project.objects.find { it.objectID == f.objectID }?.name
+                                                    ?: "?"
+                                            )
+                                        )
+                                    }
+                                    8003 -> {
+                                        val ii =
+                                            project.variables.find { it.objectIdString == f.objectID }
+                                        str.add(
+                                            Text(
+                                                ii?.name ?: f.description,
+                                                color = Color.Black
+                                            )
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Block(b: Bloc, project: Project) {
+    Box(Modifier.clip(RoundedCornerShape(8.dp))) {
+        Box(
+            Modifier
+                .background(
+                    when (blocks[b.type.toString()]!![0]) {
+                        "move" -> Color(215, 62, 30)
+                        "looks" -> Color(99, 174, 30)
+                        "draw" -> Color(166, 0, 110)
+                        "var" -> Color(231, 182, 1)
+                        "ctrl" -> Color(62, 131, 190)
+                        "old" -> Color(100, 127, 150)
+                        else -> Color(218, 218, 218)
+                    }
+                )
+                .padding(10.dp)
+        ) {
+            Row(
+                Modifier.horizontalScroll(rememberScrollState()),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(blocks[b.type.toString()]!![1], color = Color.White)
+                b.parameters?.forEach {
+                    Par(it, project, true)
+                }
+            }
+        }
     }
 }
